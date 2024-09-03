28ce45b84d3cc1deef9f2d5608939160b067be53
Manish Gautam <manishgtm123@gmail.com>
2024-09-03 09:01:47 +0545

    Added links

---
+++ components/login.php
@@ -0,0 +1,12 @@
+<head>
+    <link rel="stylesheet" href="login.css">
+</head>
+
+<div class="container">
+        <h1>Login</h1>
+        <div class="login">
+            <input type="text" placeholder="Email">
+            <br>
+            <input type="password" placeholder="Password">
+        </div>
+    </div>
